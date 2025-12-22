2c05069f2e2b4ea8dff47cdbed56d9529ab8e608
PointBreak202 <sakshamnpatil227@gmail.com>
2025-12-22 22:52:25 +0530

    Initial website upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function welcomeMessage() {
+    alert("Welcome to Club Cafe! Check out our delicious menu.");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you! We will contact you soon.");
+}
+
+function likeGallery() {
+    alert("Thanks for liking our gallery ❤️");
+}
+
+function openImage(img) {
+    window.open(img.src, "_blank");
+}
+
+
